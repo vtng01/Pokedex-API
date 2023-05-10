d177e35518a77cd33accbd7f0bfdf7022608bcc2
Van Nguyen <vanthoai@fb.com>
2023-05-10 11:10:57 +0100

    server set up

--- index.js
+++ index.js
@@ -2,7 +2,7 @@ import express, { json } from "express";
 const app = express();
 
 app.use(json());
-// app.use(express.urlencoded({ extended: true }));
+app.use(express.urlencoded({ extended: true }));
 
 // middleware section
 
