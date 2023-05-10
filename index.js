63d215d5953188d5712919772d75cf67195417e2
Van Nguyen <vanthoai@fb.com>
2023-05-10 11:07:42 +0100

    set up server

--- index.js
+++ index.js
@@ -1,1 +1,30 @@
-// code
+import express, { json } from "express";
+const app = express();
+
+app.use(json());
+// app.use(express.urlencoded({ extended: true }));
+
+// middleware section
+
+// routes
+app.get("/", async (req, res, next) => {
+  res.send(`You just called ${req.method} method`);
+});
+
+app.post("/", async (req, res, next) => {
+  res.send(`You just called ${req.method} method`);
+});
+
+app.patch("/", async (req, res, next) => {
+  res.send(`You just called ${req.method} method`);
+});
+
+app.put("/", async (req, res, next) => {
+  res.send(`You just called ${req.method} method`);
+});
+
+app.delete("/", async (req, res, next) => {
+  res.send(`You just called ${req.method} method`);
+});
+
+export default app;
--- server.js
+++ server.js
@@ -1,1 +1,8 @@
-// code
+import app from "./index.js";
+
+const PORT = 4000;
+
+app.listen(PORT, () => {
+  //   sequelize.sync({ force: false });
+  console.log(`The PokeDex API is running on http://localhost:${PORT}`);
+});
