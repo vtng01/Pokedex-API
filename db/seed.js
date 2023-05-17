4493b58c33fdd883e4092b1fbc589f331f4be9ea
Van Nguyen <vanthoai@fb.com>
2023-05-17 13:49:24 +0100

    added seed data for pokemon and call the seed function

--- db/seed.js
+++ db/seed.js
@@ -1,20 +1,31 @@
-import {db, Users, Pokedex, Logs} from "./index.js";
-import {seedData} from "./seedData.js";
+import { db, Users, Pokedex, Logs } from "./index.js";
+import { seedData, pokemonSeedData } from "./seedData.js";
 
-async function genUsers(name, password, email, occupation, isAdmin) {
-    await Users.create({
-        'name': name,
-        'password': password,
-        'email': email,
-        'occupation': occupation,
-        'isAdmin': isAdmin
-    });
+async function genUsers(usersObject) {
+  await Users.bulkCreate(usersObject);
+}
+
+async function genPokemons(pokemonObject) {
+  await Pokedex.bulkCreate(pokemonObject);
+}
+
+async function assignPokedexEntry() {
+  let pokemons = await Pokedex.findAll();
+  let professors = await Users.findAll({
+    where: {
+      occupation: "Researcher",
+    },
+  });
+
+  await professors[0].addPokedex(pokemons[0]);
+  await professors[2].addPokedex(pokemons[1]);
+  await professors[1].addPokedex(pokemons[2]);
 }
 
 export default async function seed() {
-    for await (const seedObj of seedData) {
-        await genUsers(seedObj.name, seedObj.password, seedObj.email, seedObj.occupation, seedObj.isAdmin);
-    }
+  await genUsers(seedData);
+  await genPokemons(pokemonSeedData);
+  await assignPokedexEntry();
 }
 
-export {seed};
+export { seed };
--- db/seedData.js
+++ db/seedData.js
@@ -1,27 +1,44 @@
-
-
 const seedData = [
-    {
-        name: 'Prof. Oak',
-        isAdmin: true,
-        email: 'oak@palletreseach.com',
-        occupation: 'Researcher',
-        password: 'oakLovesPokemon',
-    },
-    {
-        name: 'Prof. Smith',
-        isAdmin: false,
-        email: 'smith@pallet.edu',
-        occupation: 'Researcher',
-        password: 'iFoundCharmanderFirst'
-    },
-    {
-        name: 'Prof. Yun',
-        isAdmin: false,
-        email: 'yun@pallet.edu',
-        occupation: 'Researcher',
-        password: 'iFoundCharmanderFirstActually'
-    }
-]
+  {
+    name: "Prof. Oak",
+    isAdmin: true,
+    email: "oak@palletreseach.com",
+    occupation: "Researcher",
+    password: "oakLovesPokemon",
+  },
+  {
+    name: "Prof. Smith",
+    isAdmin: false,
+    email: "smith@pallet.edu",
+    occupation: "Researcher",
+    password: "iFoundCharmanderFirst",
+  },
+  {
+    name: "Prof. Yun",
+    isAdmin: false,
+    email: "yun@pallet.edu",
+    occupation: "Researcher",
+    password: "iFoundCharmanderFirstActually",
+  },
+];
 
-export {seedData};
+const pokemonSeedData = [
+  {
+    name: "Pikachu",
+    type: "Electric",
+    description: "The electric mouse pokemon.",
+  },
+  {
+    name: "Charizard",
+    type: "Fire/Flying",
+    description: "A fire lizard evolved to have wings. A dragon perhaps.",
+  },
+  {
+    name: "Blastoise",
+    type: "Water",
+    description:
+      "The turtle/tortoise water pokemon. Has cannons attached to its shell.",
+  },
+];
+
+export { seedData, pokemonSeedData };
